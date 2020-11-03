d03a526e3578e78a7cff7487233ab771ca741225
Samantha <samanthagottlieb31@gmail.com>
2020-11-03 15:25:21 +0000

    Add #top_up and #deduct spec tests and methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,17 @@
 class Oystercard
   attr_reader :balance
+  LIMIT = 90
 
   def initialize
     @balance = 0
+  end
+
+  def top_up(amount)
+    raise "Maximum balance of #{LIMIT} reached" if @balance + amount > LIMIT
+    @balance += amount
+  end
+
+  def deduct(fare)
+    @balance -= fare
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,13 +5,40 @@ describe Oystercard do
     expect(subject).to be_instance_of(Oystercard)
   end
 
-  context '#balance' do
+  describe '#balance' do
     it 'responds to the method' do
       expect(subject).to respond_to(:balance)
     end
 
     it 'sets a default balance of 0' do
       expect(subject.balance).to eq(0)
+    end
+  end
+
+  describe '#top_up' do
+    it 'responds to the method' do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+
+    it 'adds amount to balance' do
+      expect { subject.top_up(10) }.to change { subject.balance }.from(0).to(10)
+    end
+
+    it 'enforces a maximum balance' do
+      max_balance = Oystercard::LIMIT
+      subject.top_up(max_balance)
+      expect { subject.top_up(1) }.to raise_error("Maximum balance of #{max_balance} reached")
+    end
+  end
+
+  describe '#deduct' do
+    it 'responds to the method' do
+      expect(subject).to respond_to(:deduct).with(1).argument
+    end
+
+    it 'deducts fare from balance' do
+      subject.top_up(10)
+      expect { subject.deduct(10) }.to change { subject.balance }.by(-10)
     end
   end
 end
