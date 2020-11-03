d7761e7b3af046cde6cb01706bcfd2b1c2ed589f
Samantha <samanthagottlieb31@gmail.com>
2020-11-03 14:38:42 +0000

    Create Oystercard class and method, add #balance spec tests and  method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,17 @@
-describe Oystercard do
+require 'oystercard'
 
+describe Oystercard do
+  it 'creates an Oystercard instance' do
+    expect(subject).to be_instance_of(Oystercard)
+  end
+
+  context '#balance' do
+    it 'responds to the method' do
+      expect(subject).to respond_to(:balance)
+    end
+
+    it 'sets a default balance of 0' do
+      expect(subject.balance).to eq(0)
+    end
+  end
 end
